3e1f972d477604d1c81bccaf9626ef46de5f458b
martza <30558997+martza@users.noreply.github.com>
2020-05-09 18:44:17 +0200

    minor changes

--- Pollutants in metropolitan areas/start.py
+++ Pollutants in metropolitan areas/start.py
@@ -56,7 +56,7 @@ data = data.sort_values(by = 'DatetimeEnd')
 
 ################################################################################
 #Question: How is NO2 concentration evolving in metropolitan areas?
-#Is the NO2 concentration in acceptable levels in the altitudes where most people live?
+#Is the NO2 concentration in acceptable levels where most people live?
 #What are the factors that affect the measurements of the NO2 concentation in metropolitan areas?
 ################################################################################
 #Sampling points in metropolitan areas give the 'background data'
@@ -74,20 +74,21 @@ background_data = pd.merge(background_data, Brussels_stations, on = 'AirQualityS
 #We want to see the impact of altitude on the NO2 measurements.
 #For this purpose we group by altitude.
 background_data2 = background_data[['Concentration','Altitude']].groupby(['Altitude'], as_index = False).aggregate([np.mean,np.median]).reset_index()
-fig9, ax9 = plt.subplots()
-background_data2.plot.bar(x = 'Altitude', y = [('Concentration','mean'),('Concentration','median')], rot = 0, ax = ax9)
-ax9.legend(["mean", "median"])
-ax9.set_ylabel("NO2 Concentration")
+fig0, ax0 = plt.subplots()
+background_data2.plot.bar(x = 'Altitude', y = [('Concentration','mean'),('Concentration','median')], rot = 0, ax = ax0)
+ax0.legend(["mean", "median"])
+ax0.set_ylabel("NO2 Concentration")
 
 #We will keep the lower altitudes <=20 m. , because it is where most of the people live.
 
 background_data = background_data[background_data['Altitude']<=20]
 background_data[['Concentration','DatetimeEnd']].groupby(['DatetimeEnd'], as_index = False).count().max()
 #There is maximum 2 points per hour. For the worst-case scenario we take the maximum.
-background_data3 = background_data[['Concentration','DatetimeEnd']].groupby(['DatetimeEnd'], as_index = False).max().reset_index()
-background_data3.columns
+background_data_max = background_data[['Concentration','DatetimeEnd']].groupby(['DatetimeEnd'], as_index = False).max().reset_index()
+background_data_max.columns
 
-time = mdates.date2num(background_data3['DatetimeEnd'])
+time = mdates.date2num(background_data_max['DatetimeEnd'])
+max = background_data_max['Concentration']
 years = mdates.YearLocator()   # every year
 months = mdates.MonthLocator()  # every month
 days = mdates.DayLocator()
@@ -95,25 +96,25 @@ years_fmt = mdates.DateFormatter('%Y')
 months_fmt = mdates.DateFormatter('%m-%Y')
 
 #Plot of NO2 concentration evolution over the years 2019 and 2020
-fig8, ax8 = plt.subplots()
-ax8.plot_date(time, max, 'b-', label = 'max')
+fig, ax = plt.subplots()
+ax.plot_date(time, max, 'b-', label = 'max')
 
 # format the ticks
-ax8.xaxis.set_major_locator(months)
-ax8.xaxis.set_major_formatter(months_fmt)
-ax8.xaxis.set_minor_locator(days)
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_major_formatter(months_fmt)
+ax.xaxis.set_minor_locator(days)
 
 # round to nearest years.
-#datemin3 = min(background_data3['DatetimeEnd'])
-#datemax3 = max(background_data3['DatetimeEnd'])
-#ax8.set_xlim(datemin3, datemax3)
+#datemin = min(background_data_max['DatetimeEnd'])
+#datemax = max(background_data_max['DatetimeEnd'])
+#ax.set_xlim(datemin, datemax)
 # format the coords message box
-ax8.format_xdata = mdates.DateFormatter('%Y-%m-%d')
-ax8.grid(False)
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+ax.grid(False)
 
 # rotates and right aligns the x labels, and moves the bottom of the
 # axes up to make room for them
-fig8.autofmt_xdate()
+fig.autofmt_xdate()
 plt.legend()
 plt.figure(figsize = [15,4])
 plt.show()
@@ -121,59 +122,70 @@ plt.show()
 #In the metropolitan Brussels area the hourly NO2 measurements do not exceed
 #the acceptable limits of 200
 
-background_data3['year'] = pd.DatetimeIndex(background_data3['DatetimeEnd']).year
-background_data3['month'] = pd.DatetimeIndex(background_data3['DatetimeEnd']).month
-background_data3['day'] = pd.DatetimeIndex(background_data3['DatetimeEnd']).day
-background_data3['hour'] = pd.DatetimeIndex(background_data3['DatetimeEnd']).hour
-
-background_data3['weekday'] = background_data3['DatetimeEnd'].dt.dayofweek
-#background_data3.columns
+#Some statistics on the dataset:
+background_data_max['Concentration'].plot.hist()
+background_data_max['Concentration'].describe()
 
 
+#Now we add to the dataset year, day, month, hour, weekday columns
+background_data_max['year'] = pd.DatetimeIndex(background_data_max['DatetimeEnd']).year
+background_data_max['month'] = pd.DatetimeIndex(background_data_max['DatetimeEnd']).month
+background_data_max['day'] = pd.DatetimeIndex(background_data_max['DatetimeEnd']).day
+background_data_max['hour'] = pd.DatetimeIndex(background_data_max['DatetimeEnd']).hour
+background_data_max['weekday'] = background_data_max['DatetimeEnd'].dt.dayofweek
 
-background_data3['Concentration'].plot.hist()
-background_data3['Concentration'].describe()
-
-# Here I aggregate the NO2 concentration per year and per month as a mean and compare amongst years 2019 and 2020
-NO2_per_year = background_data3[background_data3['month']<5][['Concentration','year']].groupby(['year']).mean() #Comparison between 2019 and 2020
+#We aggregate the NO2 concentration per year and per month as a mean and compare years 2019 and 2020
+NO2_per_year = background_data_max[background_data_max['month']<6][['Concentration','year']].groupby(['year']).mean()
 fig4, ax4 = plt.subplots()
 NO2_per_year['Concentration'].plot.bar()
 ax4.legend(["mean"])
 ax4.set_ylabel("NO2 Concentration")
 plt.savefig('NO2_Brussels_per_year.png')
-#For the first four months of 2019 and 2020 the mean concentration of NO2 presents a drop of 30% and this can be seen as an effect of the COVID-19 crisis in belgium
-NO2_per_month = background_data3[['Concentration','year','month']].groupby(['year','month']).mean()
+
+#For the first four months of 2019 and 2020 the mean concentration of NO2 presents a drop of 30%.
+#This can be interpreted as an effect of the COVID-19 crisis in brussels
+
+NO2_per_month = background_data_max[['Concentration','year','month']].groupby(['year','month']).mean()
 NO2_per_month['Concentration'].plot.bar()
+#The biggest drop can be seen in the month of February, where the NO2 concentration fell in half the one of 2019
 
-# Define categorical variables: a. Seasons b. Weekdays [Monday-Sunday] d. Is public Holiday [True, False]
-background_data3['season'] = ''
-background_data3.loc[background_data3['month'].isin([12,1,2]),'season'] = 'winter'
-background_data3.loc[background_data3['month'].isin([3,4,5]),'season'] = 'spring'
-background_data3.loc[background_data3['month'].isin([6,7,8]),'season'] = 'summer'
-background_data3.loc[background_data3['month'].isin([9,10,11]),'season'] = 'autumn'
-background_data3.to_csv('NO2_Brussels.csv',index = False)
-
-NO2_per_hour = background_data3[['DatetimeEnd','Concentration','hour']].groupby(['hour'], as_index = False).aggregate([np.mean, np.median]).reset_index()
+#Explore the evolution of the concentration of NO2 with the hour of the day
+NO2_per_hour = background_data_max[['DatetimeEnd','Concentration','hour']].groupby(['hour'], as_index = False).aggregate([np.mean, np.median]).reset_index()
 NO2_per_hour.columns
 fig1, ax1 = plt.subplots()
 NO2_per_hour.plot.bar(x = 'hour', y = [('Concentration','mean'),('Concentration','median')], rot = 0, ax = ax1)
 ax1.legend(["mean", "median"])
 ax1.set_ylabel("NO2 Concentration")
 plt.savefig('NO2_Brussels_per_hour.png')
-# There is a certain behaviour of the concentration regarding the time of the day
-#NO2_per_hour
-NO2_per_weekday = background_data3[['DatetimeEnd','Concentration','weekday']].groupby(['weekday'], as_index = False).aggregate([np.mean, np.median]).reset_index()
+#The graph shows the evolution of the concentration with the hour of the day.
+#There is a pattern, which is probably related to the working habbits of people.
+
+#Explore the evolution of the concentration of NO2 with the weekday
+NO2_per_weekday = background_data_max[['DatetimeEnd','Concentration','weekday']].groupby(['weekday'], as_index = False).aggregate([np.mean, np.median]).reset_index()
 fig2, ax2 = plt.subplots()
 NO2_per_weekday.plot.bar(x = 'weekday', y = [('Concentration','mean'),('Concentration','median')], rot = 0, ax = ax2)
 ax2.legend(["mean", "median"])
 ax2.set_ylabel("NO2 Concentration")
 plt.savefig('NO2_Brussels_per_weekday.png')
+# There is a drop of the concentration of NO2 on Saturdays and Sundays
 
-# As expected the cencentration on saturdays and Sundays is falling
-NO2_per_season = background_data3[['DatetimeEnd','Concentration','season']].groupby(['season'], as_index = False).aggregate([np.mean, np.median]).reset_index()
+# Define a new categorical variable for the seasons
+background_data_max['season'] = ''
+background_data_max.loc[background_data_max['month'].isin([12,1,2]),'season'] = 'winter'
+background_data_max.loc[background_data_max['month'].isin([3,4,5]),'season'] = 'spring'
+background_data_max.loc[background_data_max['month'].isin([6,7,8]),'season'] = 'summer'
+background_data_max.loc[background_data_max['month'].isin([9,10,11]),'season'] = 'autumn'
+background_data_max.to_csv('NO2_Brussels.csv',index = False)
+
+NO2_per_season = background_data_max[['DatetimeEnd','Concentration','season']].groupby(['season'], as_index = False).aggregate([np.mean, np.median]).reset_index()
 fig3, ax3 = plt.subplots()
 NO2_per_season.plot.bar(x = 'season', y = [('Concentration','mean'),('Concentration','median')], rot = 0, ax = ax3)
 ax3.legend(["mean", "median"])
 ax3.set_ylabel("NO2 Concentration")
 plt.savefig('NO2_Brussels_per_season.png')
-#As expected NO2 falls in the summer and is increasing during the winter.
+#There is a drop of the concentration of NO2 the summer and an increase in the winter.
+
+################################################################################
+#Additional variables that can affect the concentration of NO2 are: season,
+#hour of the day, day of the week, social and political factors
+################################################################################
